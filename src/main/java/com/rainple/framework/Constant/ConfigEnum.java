0e1d2176a3ef101ab8ef093b311e110536dc9e26
rainplewu <1170971295@qq.com>
2018-10-01 01:34:13 +0800

    修改bug

--- src/main/java/com/rainple/framework/Constant/ConfigEnum.java
+++ src/main/java/com/rainple/framework/Constant/ConfigEnum.java
@@ -3,6 +3,7 @@ package com.rainple.framework.Constant;
 public enum ConfigEnum {
     DISPATCHER_PREFIX("com.rainple.dispatcher.prefix"),
     DISPATCHER_SUBFIX("com.rainple.dispatcher.subfix"),
+    DISPATCHER_SUBFIX1("com.rainple.dispatcher.subfix"),
     SCANPACKAGE("com.rainple.scanPack");
     private String name;
     ConfigEnum(String name){
