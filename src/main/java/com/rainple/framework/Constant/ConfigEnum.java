158d0d62d746af6d7f757398fe9f8b09b97c1fae
rainplewu <1170971295@qq.com>
2018-10-01 01:12:20 +0800

    增加常量

--- src/main/java/com/rainple/framework/Constant/ConfigEnum.java
+++ src/main/java/com/rainple/framework/Constant/ConfigEnum.java
@@ -3,7 +3,8 @@ package com.rainple.framework.Constant;
 public enum ConfigEnum {
     DISPATCHER_PREFIX("com.rainple.dispatcher.prefix"),
     DISPATCHER_SUBFIX("com.rainple.dispatcher.subfix"),
-    SCANPACKAGE("com.rainple.scanPack");
+    SCANPACKAGE("com.rainple.scanPack"),
+    SCAN("com.rainple.scanPack");
     private String name;
     ConfigEnum(String name){
         this.name = name;
