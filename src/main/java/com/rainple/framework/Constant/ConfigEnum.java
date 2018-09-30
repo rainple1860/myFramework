04480aeae466fc00eda42c327f145be4ccba9fc7
rainplewu <39738078+rainplewu@users.noreply.github.com>
2018-10-01 01:15:04 +0800

    删除常量

--- src/main/java/com/rainple/framework/Constant/ConfigEnum.java
+++ src/main/java/com/rainple/framework/Constant/ConfigEnum.java
@@ -4,7 +4,6 @@ public enum ConfigEnum {
     DISPATCHER_PREFIX("com.rainple.dispatcher.prefix"),
     DISPATCHER_SUBFIX("com.rainple.dispatcher.subfix"),
     SCANPACKAGE("com.rainple.scanPack"),
-    SCAN("com.rainple.scanPack");
     private String name;
     ConfigEnum(String name){
         this.name = name;
